3583335616b38292cd28b706085fb21b9ccabbd2
farkhan432 <achamdfarhan0101@gmail.com>
2021-04-07 16:35:32 +0700

    project

---
+++ src/components/Box.vue
@@ -0,0 +1,45 @@
+<template>
+    <b-container-fluid class="bv-example-row">
+  <b-row class="text" align-h="">
+        <b-col class=" box bg-danger" cols="4">One of two columns</b-col> 
+        <b-col class="box bg-warning" cols="4">One of two columns</b-col>
+        <b-col class="box bg-primary"  cols="4">One of two columns</b-col>
+  </b-row>
+
+ 
+
+  <b-row align-h="end">
+    <b-col cols="4">One of two columns</b-col>
+    <b-col cols="4">One of two columns</b-col>
+  </b-row>
+
+  <b-row align-h="around">
+    <b-col cols="4">One of two columns</b-col>
+    <b-col cols="4">One of two columns</b-col>
+  </b-row>
+
+  <b-row align-h="between">
+    <b-col cols="4">One of two columns</b-col>
+    <b-col cols="4">One of two columns</b-col>
+  </b-row>
+</b-container-fluid>
+</template>
+
+<script>
+export default{
+    data(){
+       return{
+
+       };
+    },
+}
+</script>
+
+<style scoped>
+.box {
+   height: 100px;
+}
+.text{
+    text-align: center;
+}
+</style>
