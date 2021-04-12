df1488051db2e56aede8fc60401f9983934743f5
farkhan432 <achamdfarhan0101@gmail.com>
2021-04-12 16:46:03 +0700

    project

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,12 +1,120 @@
-<template>
-    <b-container class="bv-example-row">
-        <b-row align-h="start">
-          <div class="col align-self-center">
-          <b-col cols="12" class="box bg-danger"><img src="../assets/logo.png"></b-col>
+<template> 
+    <div> 
+        <b-container fluid>
+            <section class="">
+      <div class="row">
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            data-ripple-color="light">
+            <img
+              src="../assets/gambar.jpg"
+              class="w-100"/>
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);">
+               
+                <div class="box2 bg-warning">
+                    <b-row align-h="between">
+                        <b-col >One of two columns</b-col>
+                        <b-col class=" d-block justify-items-right" >One</b-col>
+                    </b-row>
+                </div>
+                
+              </div>
+            </a>
           </div>
-        </b-row>
-
-    </b-container>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/111.jpg"
+              class="w-100"/>
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);">
+                <p class="teks">ayo</p>
+                </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/112.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/114.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/115.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/116.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+        </b-container>
+</div>
+    
 </template>
 
 <script>
@@ -21,8 +129,24 @@ export default{
 
 <style scoped>
 .box {
-   height: 300px;
-   text-align: center;
+    height: 300px;
 }
+.box2{
+    height: 100px;
+}
+.gambar{
+    height: 120px;
+    width: 300px;
+}
+.teks{
+    color: black;
+}
+.button{
+    border:;
+    background:none;
+   
+  
+}
+
 
 </style>
